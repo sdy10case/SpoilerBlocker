ede0f1c26e18cfbb1d6e72dbe30e9f89b0ebc7f7
Eli Gottlieb <36972688+EliGottlieb@users.noreply.github.com>
2022-04-10 19:13:35 -0400

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,1 +1,9 @@
+function login() {
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    chrome.runtime.sendMessage(
+        { from: "login", action: [email, password] },
+    );
+}
 
+document.getElementById('Submit').onclick = login
