e90ca1fd4fc46d618f15ffbdcbdeafa0f6f92d16
Eli Gottlieb <36972688+EliGottlieb@users.noreply.github.com>
2022-04-18 12:36:48 -0400

    Update content_script.js

--- content_script.js
+++ content_script.js
@@ -1,12 +1,6 @@
 const text = document.querySelectorAll('h1, h2, h3, h4, h5, p, li, td, caption, span, a, code')
-
 const imgs = document.getElementsByTagName('img')
 let imgFile = "Icons/fullx_monkey - Copy ("
-//creates a constant variable text that contains all possible text on the document (webpage) and puts it into an array of different text elements
-//for (elt of imgs)
-  //console.log(imgs)
-
-let idAndPH = []
 
 chrome.runtime.sendMessage({ from: "clist", action: [document.location.href] }, function (response) {
   console.log(response)
@@ -15,40 +9,32 @@ chrome.runtime.sendMessage({ from: "clist", action: [document.location.href] }, 
     console.log(clist)
     for (elt of text) {
       for (let i = 0; i < clist.length; i++) {
-        //console.log(clist.length + " " + i)
-        //console.log(clist[i].identifer)
-        //console.log(clist[i].placeholder)
-        //console.log("Replaced: " + clist[i].identifier + " With: " + clist[i].placeholder)
         if (clist[i].isBlocked)
-          elt.innerHTML = elt.innerHTML.replace(clist[i].identifier, clist[i].placeholder)
+          elt.innerHTML = elt.innerHTML = elt.innerHTML.replace(clist[i].identifier, '<span style="background-color:powderblue;">' + clist[i].placeholder + '</span> <!--monkey monkey-->')
       }
       for (elt of imgs) {
 				let j = 2
         for (let i = 0; i < clist.length; i++) {
           if (clist[i].isBlocked && clist[i].identifier == elt.src) {
-            let source = imgFile + j + ").png"
+            source = imgFile + j + ").png"
             let url = chrome.extension.getURL(source);
-            oldAlt = elt.alt
             elt.src = url;
-						elt.id = clist[i].placeholder
-            source = source.replaceAll(" ", "%20")
-            console.log(source)
-            
-            chrome.storage.local.set({source: oldAlt})
+            url = url.replaceAll(" ", "%20")
+            clist[i].placeholder = url
+            oldAlt = elt.alt
+            elt.alt = clist[i].reason
+            console.log(url)
+            console.log(oldAlt)
+            var obj = {};
+            obj[url] = oldAlt
+            chrome.storage.local.set(obj)
             console.log("Image replaced")
 						j++
           }
         }
       }
+      var packedClist = {"content": clist}
+      chrome.storage.local.set({'clist' : packedClist})
     }
   })
 })
-
-
-/*chrome.storage.local.get('key', function(result){
-    let x = result.key;
-    //runs through the text elements in text and replaces specific things.
-    for(elt of text){
-
-        elt.innerHTML = elt.innerHTML.replace(x, "ASDINAODSIA")*/
-       // elt.innerHTML = elt.innerHTML.replace("is", "REPLACED")
