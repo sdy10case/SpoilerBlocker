a80b5bdaf26d38510f7bc8f1fa71059579e4bb3d
Eli Gottlieb <36972688+EliGottlieb@users.noreply.github.com>
2022-04-18 12:38:53 -0400

    Update popup.js

--- popup.js
+++ popup.js
@@ -4,6 +4,40 @@ function login() {
     chrome.runtime.sendMessage(
         { from: "login", action: [email, password] },
     );
+    window.close()
 }
 
-document.getElementById('Submit').onclick = login
+function register() {
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    var name = document.getElementById('name').value;
+    var surname = document.getElementById('surname').value;
+    chrome.runtime.sendMessage(
+        { from: "register", action: [email, password, name, surname] },
+    );
+    window.close()
+}
+
+function logout() {
+	chrome.runtime.sendMessage(
+        { from: "logout", action: ["I would like to logout"] },
+    );
+    window.close()
+}
+
+try {
+    document.getElementById('Login').onclick = login;
+} catch (error) {
+    console.log(error)
+}
+try {
+	document.getElementById('Register').onclick = register;
+} catch (error) {
+    console.log(error)
+}
+try {
+	document.getElementById('Logout').onclick = logout;
+} catch (error) {
+    console.log(error)
+}
+
